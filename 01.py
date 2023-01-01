7416031ed44b9ae18ecc4a6652475e051aea2f8d
saumya9a <106297354+saumya9a@users.noreply.github.com>
2023-01-01 17:12:25 +0530

    file1

---
+++ 01.py
@@ -0,0 +1,22 @@
+#Print all family members name
+
+fatherName ='shyam sundar pandey'
+motherName ='Sumshila pandey'
+sisterName = 'Khushboo' 
+brotherName = 'aditya'
+# print(fatherName+','+motherName+','+sisterName+','+brotherName)
+
+
+#Print all family members name in single line
+
+fatherName,motherName,sisterName,brotherName   ='shyam sundar pandey','Sushila pandey','Khushboo', 'aditya'
+
+
+print(fatherName+','+motherName+','+sisterName+','+brotherName)
+#sistersDetails 
+sisterName1='Khushboo'
+age = 28
+gender = 'F'
+maritalStatus = 'Married'
+# print(sisterName1,age,gender,maritalStatus)
+print('My sister name is '+sisterName1+' her age is '+str(age)+' she is '+maritalStatus)
